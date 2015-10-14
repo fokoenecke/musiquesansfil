ad4ed5354b67bd0d05127a738e43d17cf71c7a89
Ole Koenecke <ole.koenecke@gpi-consulting.de>
2015-10-14 15:39:42 +0200

    added filter for server IP address

--- msf.go
+++ msf.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/google/gopacket"
@@ -13,48 +14,71 @@ import (
 
 var serverHost = flag.String("s", "127.0.0.1", "Set Pure Data server host")
 var serverPort = flag.Int("p", 9001, "Set Pure Data server port")
+var device = flag.String("d", "wlan0", "Set device to listen on")
 
 func main() {
 	flag.Parse()
-	if handle, err := pcap.OpenLive("wlan0", 65536, true, 0); err != nil {
+
+	devices, err := pcap.FindAllDevs()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	filter := ""
+	for _, dev := range devices {
+		if dev.Name == *device {
+			for _, address := range dev.Addresses {
+				filter = fmt.Sprintf("not ip host %s ", address.IP)
+				break
+			}
+		}
+	}
+
+	handle, err := pcap.OpenLive(*device, 65536, true, 0)
+	if err != nil {
 		defer handle.Close()
 		panic(err)
-	} else {
-		fmt.Println(handle.LinkType())
-		packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-		client := osc.NewClient(*serverHost, *serverPort)
+	}
 
-		for packet := range packetSource.Packets() {
-			go playSound(client)
-			// Let's see if the packet is an ethernet packet
-			ethernetLayer := packet.Layer(layers.LayerTypeEthernet)
-			if ethernetLayer != nil {
-				fmt.Println("Ethernet layer detected.")
-				ethernetPacket, _ := ethernetLayer.(*layers.Ethernet)
-				fmt.Println("Source MAC: ", ethernetPacket.SrcMAC)
-				fmt.Println("Destination MAC: ", ethernetPacket.DstMAC)
-				// Ethernet type is typically IPv4 but could be ARP or other
-				fmt.Println("Ethernet type: ", ethernetPacket.EthernetType)
-			}
+	err = handle.SetBPFFilter(filter)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-			ip4Layer := packet.Layer(layers.LayerTypeIPv4)
-			if ip4Layer != nil {
-				var ip = ip4Layer.(*layers.IPv4)
-				fmt.Println("Source IP: ", ip.SrcIP)
-				fmt.Println("Destination IP: ", ip.DstIP)
-			}
-			
-			fmt.Println()
+	fmt.Println(handle.LinkType())
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	client := osc.NewClient(*serverHost, *serverPort)
+
+	for packet := range packetSource.Packets() {
+		fmt.Println(packet)
+		go playSound(client)
+		// Let's see if the packet is an ethernet packet
+		ethernetLayer := packet.Layer(layers.LayerTypeEthernet)
+		if ethernetLayer != nil {
+			fmt.Println("Ethernet layer detected.")
+			ethernetPacket, _ := ethernetLayer.(*layers.Ethernet)
+			fmt.Println("Source MAC: ", ethernetPacket.SrcMAC)
+			fmt.Println("Destination MAC: ", ethernetPacket.DstMAC)
+			// Ethernet type is typically IPv4 but could be ARP or other
+			fmt.Println("Ethernet type: ", ethernetPacket.EthernetType)
 		}
+
+		ip4Layer := packet.Layer(layers.LayerTypeIPv4)
+		if ip4Layer != nil {
+			var ip = ip4Layer.(*layers.IPv4)
+			fmt.Println("Source IP: ", ip.SrcIP)
+			fmt.Println("Destination IP: ", ip.DstIP)
+		}
+		fmt.Println()
 	}
 }
 
 func playSound(client *osc.Client) {
-	msg := osc.NewMessage("/midi/noteon")
-	msg.Append(int32(64))
+	msg := osc.NewMessage("/instrument/kick")
+	msg.Append(int32(1))
 	client.Send(msg)
 	time.Sleep(50 * time.Millisecond)
-	msg = osc.NewMessage("/midi/noteoff")
-	msg.Append(int32(64))
+	msg = osc.NewMessage("/instrument/kick")
+	msg.Append(int32(1))
 	client.Send(msg)
 }
