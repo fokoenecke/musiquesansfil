7a8bc09ec77648fb191600e0384d945c44c51fe2
Ole Koenecke <ole.koenecke@gpi-consulting.de>
2015-11-17 14:16:10 +0100

    improved socket.io server + refactoring

--- extra/test.go
+++ extra/test.go
@@ -3,16 +3,59 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"math/rand"
+	"net/http"
+	"text/template"
 	"time"
 
+	"github.com/googollee/go-socket.io"
 	"github.com/hypebeast/go-osc/osc"
 )
 
 var serverHost = flag.String("s", "192.168.178.20", "Set Pure Data server host")
 var serverPort = flag.Int("p", 9001, "Set Pure Data server port")
 
+type data struct{}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("../tmpl/index.html")
+
+	val := data{}
+	t.Execute(w, val)
+}
+
 func main() {
+
+	server, err := socketio.NewServer(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	server.On("connection", func(so socketio.Socket) {
+		log.Println("on connection")
+		so.Join("chat")
+		so.On("chat message", func(msg string) {
+			fmt.Println(so, msg)
+			log.Println("emit:", so.Emit("chat message", msg))
+			so.BroadcastTo("chat", "chat message", msg)
+		})
+		so.On("disconnection", func() {
+			log.Println("on disconnect")
+		})
+	})
+	server.On("error", func(so socketio.Socket, err error) {
+		log.Println("error:", err)
+	})
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	mux.Handle("/socket.io/", server)
+	log.Println("Serving at localhost:5000...")
+	log.Fatal(http.ListenAndServe(":5000", mux))
+
+}
+
+func main2() {
 
 	client := osc.NewClient(*serverHost, *serverPort)
 	fmt.Println("server:", *serverHost, "port:", *serverPort)
--- msf.go
+++ msf.go
@@ -4,13 +4,16 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
 	"sort"
 	"sync"
+	"text/template"
 	"time"
 
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
+	"github.com/googollee/go-socket.io"
 	"github.com/hypebeast/go-osc/osc"
 )
 
@@ -18,23 +21,150 @@ var serverHost = flag.String("s", "127.0.0.1", "Set Pure Data server host")
 var serverPort = flag.Int("p", 9001, "Set Pure Data server port")
 var device = flag.String("d", "wlan0", "Set device to listen on")
 
-type Activity struct {
-	packets    int
-	sizeSum    int
-	since      time.Time
-	instrument int
+type activity struct {
+	packets      int
+	sizeSum      int
+	since        time.Time
+	instrument   int
+	currentLevel int
 }
 
-func (activity *Activity) increment() {
+func (activity *activity) increment() {
 	activity.packets++
 }
 
-func (activity *Activity) currentPackets() int {
+func (activity *activity) currentPackets() int {
 	return activity.packets
 }
 
-func (activity *Activity) addPacketSize(size int) {
+func (activity *activity) addPacketSize(size int) {
 	activity.sizeSum += size
+}
+
+type instrument struct {
+	name               string
+	mapLevel           func(bps float64) int
+	adjustCurrentLevel func(client *activity, targetLevel int)
+	sendMessage        func(client *osc.Client, level int, speed int, instrument string)
+}
+
+func mapSpeedLevel(pps float64) int {
+	var level int
+	if pps > 6 {
+		level = 4
+	} else if pps > 3 {
+		level = 3
+	} else if pps > 2 {
+		level = 2
+	} else if pps > 1 {
+		level = 1
+	} else if pps > .5 {
+		level = 8
+	} else {
+		level = 16
+	}
+	return level
+}
+
+func mapDrumLevel(bps float64) int {
+	var level int
+	if bps > 150 {
+		level = 4
+	} else if bps > 120 {
+		level = 3
+	} else if bps > 70 {
+		level = 2
+	} else if bps > 30 {
+		level = 1
+	} else if bps > 15 {
+		level = 8
+	} else if bps > 5 {
+		level = 16
+	} else {
+		level = 0
+	}
+	return level
+}
+
+func mapMelodyLevel(bps float64) int {
+	var level int
+	if bps > 150 {
+		level = 8
+	} else if bps > 130 {
+		level = 7
+	} else if bps > 100 {
+		level = 6
+	} else if bps > 80 {
+		level = 5
+	} else if bps > 40 {
+		level = 4
+	} else if bps > 30 {
+		level = 3
+	} else if bps > 10 {
+		level = 2
+	} else if bps > 5 {
+		level = 1
+	} else {
+		level = 0
+	}
+	return level
+}
+
+func adjustDrumLevel(client *activity, targetLevel int) {
+	if targetLevel > 4 {
+		client.currentLevel = targetLevel
+	} else {
+		if client.currentLevel > 4 {
+			client.currentLevel = 1
+		}
+		adjustLevel(client, targetLevel)
+	}
+}
+
+func adjustMelodyLevel(client *activity, targetLevel int) {
+	client.currentLevel = targetLevel
+}
+
+func adjustLevel(client *activity, targetLevel int) {
+	if client.currentLevel < targetLevel {
+		client.currentLevel++
+	} else if client.currentLevel > targetLevel {
+		client.currentLevel--
+	}
+}
+
+type data struct{}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("tmpl/index.html")
+
+	val := data{}
+	t.Execute(w, val)
+}
+
+func serv() *socketio.Server {
+
+	server, err := socketio.NewServer(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	server.On("connection", func(so socketio.Socket) {
+		log.Println("on connection")
+		so.Join("chat")
+		so.On("disconnection", func() {
+			log.Println("on disconnect")
+		})
+	})
+	server.On("error", func(so socketio.Socket, err error) {
+		log.Println("error:", err)
+	})
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	mux.Handle("/socket.io/", server)
+	go http.ListenAndServe(":8000", mux)
+
+	return server
 }
 
 func main() {
@@ -73,51 +203,49 @@ func main() {
 
 	var clients = struct {
 		sync.RWMutex
-		m              map[string]*Activity
+		m              map[string]*activity
 		instrumentPool []int
-	}{m: make(map[string]*Activity)}
+	}{m: make(map[string]*activity)}
 
-	instruments := map[int]string{
-		0: "kick",
-		1: "snare",
-		2: "hh",
-		3: "bass",
+	instruments := map[int]*instrument{
+		0: &instrument{"kick", mapDrumLevel, adjustDrumLevel, sendDrumMessage},
+		1: &instrument{"snare", mapDrumLevel, adjustDrumLevel, sendDrumMessage},
+		2: &instrument{"hh", mapDrumLevel, adjustDrumLevel, sendDrumMessage},
+		3: &instrument{"bass", mapMelodyLevel, adjustMelodyLevel, sendMelodyMessage},
+		4: &instrument{"melody", mapMelodyLevel, adjustMelodyLevel, sendMelodyMessage},
 	}
 
+	server := serv()
+	ticker := time.NewTicker(time.Second * 2)
 	go func() {
-		for {
-
+		for t := range ticker.C {
+			fmt.Println("Tick at", t)
 			clients.Lock()
-
 			for key, value := range clients.m {
 				elapsed := time.Since(value.since)
 				pps := float64(value.currentPackets()) / elapsed.Seconds()
 				bps := float64(value.sizeSum) / elapsed.Seconds()
+
 				instrument, ok := instruments[value.instrument]
 				if ok {
-					if bps > 200 {
-						sendMessage(client, 4, instrument)
-					} else if bps > 150 {
-						sendMessage(client, 3, instrument)
-					} else if bps > 50 {
-						sendMessage(client, 2, instrument)
-					} else if bps > 20 {
-						sendMessage(client, 1, instrument)
-					} else {
-						sendMessage(client, 0, instrument)
+					targetLevel := instrument.mapLevel(bps)
+					if targetLevel == 0 {
 						clients.instrumentPool = append(clients.instrumentPool, value.instrument)
 						sort.Ints(clients.instrumentPool)
 						delete(clients.m, key)
 					}
+					speed := mapSpeedLevel(pps)
+					instrument.adjustCurrentLevel(value, targetLevel)
+					instrument.sendMessage(client, value.currentLevel, speed, instrument.name)
 				}
 				fmt.Println("Key:", key, "instrument:", value.instrument, "elapsed:", elapsed, "Packets:", value.currentPackets(), "pps:", pps, "bps:", bps)
+				server.BroadcastTo("chat", "chat message", key)
 			}
 			if len(clients.m) > 0 {
 				fmt.Println()
 			}
 
 			clients.Unlock()
-			time.Sleep(2 * time.Second)
 		}
 	}()
 
@@ -149,7 +277,7 @@ func main() {
 				} else {
 					instrument = len(clients.m)
 				}
-				clients.m[ethernetPacket.SrcMAC.String()] = &Activity{1, packetLength, time.Now(), instrument}
+				clients.m[ethernetPacket.SrcMAC.String()] = &activity{1, packetLength, time.Now(), instrument, 0}
 			}
 			_, ok = clients.m[ethernetPacket.DstMAC.String()]
 			if ok {
@@ -164,7 +292,7 @@ func main() {
 				} else {
 					instrument = len(clients.m)
 				}
-				clients.m[ethernetPacket.DstMAC.String()] = &Activity{1, packetLength, time.Now(), instrument}
+				clients.m[ethernetPacket.DstMAC.String()] = &activity{1, packetLength, time.Now(), instrument, 0}
 			}
 			clients.Unlock()
 
@@ -181,19 +309,17 @@ func main() {
 	}
 }
 
-func sendMessage(client *osc.Client, level int, instrument string) {
+func sendDrumMessage(client *osc.Client, level int, speed int, instrument string) {
 	msg := osc.NewMessage("/instrument/" + instrument)
 	fmt.Println("sending", level, "to", instrument)
 	msg.Append(int32(level))
 	client.Send(msg)
 }
 
-func playSound(client *osc.Client) {
-	msg := osc.NewMessage("/instrument/kick")
-	msg.Append(int32(1))
-	client.Send(msg)
-	time.Sleep(50 * time.Millisecond)
-	msg = osc.NewMessage("/instrument/kick")
-	msg.Append(int32(1))
+func sendMelodyMessage(client *osc.Client, level int, speed int, instrument string) {
+	msg := osc.NewMessage("/instrument/" + instrument)
+	fmt.Println("sending", level, ",", speed, "to", instrument)
+	msg.Append(int32(level))
+	msg.Append(int32(speed))
 	client.Send(msg)
 }
